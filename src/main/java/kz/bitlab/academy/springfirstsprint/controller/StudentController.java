862271c1f4b94839202b15031bd31c29bd5c86fa
Asy1bek <136853235+Asy1bek@users.noreply.github.com>
2024-04-01 13:09:31 +0600

    Update StudentController.java

--- src/main/java/kz/bitlab/academy/springfirstsprint/controller/StudentController.java
+++ src/main/java/kz/bitlab/academy/springfirstsprint/controller/StudentController.java
@@ -1,6 +1,8 @@
 package kz.bitlab.academy.springfirstsprint.controller;
 
+import kz.bitlab.academy.springfirstsprint.entity.StudentEntity;
 import kz.bitlab.academy.springfirstsprint.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class StudentController {
 
+    @Autowired
+    private StudentService studentService ;
+
     @GetMapping("/")
     public String indexPage(Model model) {
-        model.addAttribute("students", StudentService.findAll());
+        model.addAttribute("students", studentService.findAll());
         return "index";
     }
 
+    @GetMapping("/addStudentForm")
+    public String showAddStudentForm() {
+        return "add-student";
+    }
+
+    @PostMapping("/addStudent")
+    public String addStudent(@RequestParam String name, @RequestParam String surname, @RequestParam int exam, @RequestParam String mark) {
+        StudentEntity student = new StudentEntity();
+        student.setName(name);
+        student.setSurname(surname);
+        student.setExam(exam);
+        student.setMark(mark);
+        studentService.save(student);
+        return "redirect:/";
+    }
 }
